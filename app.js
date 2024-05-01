6310f3f4e7df2939b47c311fd2aca690d9f01ba0
mariovaquero <mvaquero143@gmail.com>
2024-04-30 21:41:04 -0400

    got busy only basic server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express();
+
+
+// routes
+
+app.get('/hello', (req,res) => {
+    res.send('Task Manager App')
+})
+
+
+
+const port = 5000
+
+app.listen(port, console.log(`server is listening on port: ${port}...`))
